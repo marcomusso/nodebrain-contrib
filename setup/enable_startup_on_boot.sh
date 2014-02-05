1d56e5e4bd335a3a9ad6c2f4826c8b837763b5c4
Marco Musso <marcomusso@users.noreply.github.com>
2014-02-05 16:34:15 +0100

    getopts and some checks in enable_startup_on_boot.sh

--- setup/enable_startup_on_boot.sh
+++ setup/enable_startup_on_boot.sh
@@ -1,20 +1,40 @@
 #!/bin/sh
+#
+# 1. copy nodebrain.service to /etc/init.d/ for every caboodle/agent specified
+# 2. add the agents with chkconfig (you can later choose the runlevels, defaults: 2345)
+# 3. create the /etc/sysconfig/ with some variables
 
-# list you agents names here, space separated
-AGENTS="<agent names>"
-# full path of caboodle
-CABOODLE="/your/caboodle/full/path/here"
-# additional variables needed by the agent, for example ORACLE_HOME/LD_LIBRARY_PATH/...
-#EXTRA_VAR="export ORACLE_HOME=/usr/lib/oracle/12.1/client64\nexport LD_LIBRARY_PATH=/usr/lib/oracle/12.1/client64/lib"
-# arguments for the daemon function, for example to run agents as another user
-#DAEMON_ARGS="export DAEMON_ARGS=\"--user=<agents user>\""
+usage() {
+  echo "Usage: $0 -c <caboodle full path> -a \"<agent1> <agent2> <...>\" [-u <user>]"
+  echo "      if there are multiple agents quote them..."
+  exit 1
+}
 
-[ "x$USER" != "xroot" ] && (echo "This script should be run as root."; exit 1)
-[ ! -d "$CABOODLE/setup" ] && (echo "The $CABOODLE/setup directory doesn't exists."; exit 1)
-# I choose to store the pid file inside the caboodle to avoid tampering /var/run with user-writable dirs
-[ ! -d "$CABOODLE/var/run" ] && (echo "The $CABOODLE/var/run directory doesn't exists.\nCreate the directory with the correct user/permissions"; exit 1)
+# process command line options
+while getopts "hv?a:b:c:u:" opt; do
+    case $opt in
+        a )      AGENTS=$OPTARG;      ;;
+        c )      CABOODLE=$OPTARG;    ;;
+        u )      RUNAS=$OPTARG;       ;;
+        h | \? ) usage;               ;;
+    esac
+done
+shift $(($OPTIND - 1))
 
-echo "Enabling startup on boot for agents $AGENTS of caboodle $CABOODLE."
+# no mandatory parameters? bail out
+[ "x$CABOODLE" = "x" -o "x$AGENTS" = "x" ] && usage
+# let's fallback to root if RUNAS is empty
+[ "x$RUNAS" = "x" ] && RUNAS=root
+# additional variables needed by the agents, ie ORACLE_HOME/LD_LIBRARY_PATH/...
+EXTRA_VAR="export ORACLE_HOME=/usr/lib/oracle/12.1/client64\nexport LD_LIBRARY_PATH=/usr/lib/oracle/12.1/client64/lib"
+# arguments for the daemon function, for example to run nb as another user
+DAEMON_ARGS="export DAEMON_ARGS=\"--user=$RUNAS\""
+
+[[ "x$USER" != "xroot" ]] && { echo "This script should be run as root."; exit 1; }
+[[ ! -d "$CABOODLE/setup" ]] && { echo "The $CABOODLE/setup directory doesn't exists."; exit 1; }
+[[ ! -d "$CABOODLE/var/run" ]] && { echo "The $CABOODLE/var/run directory doesn't exists.\nCreate the directory with the correct user/permissions"; exit 1; }
+
+echo "Enabling startup on boot for agents $AGENTS of caboodle $CABOODLE (as user $RUNAS)."
 
 cd $CABOODLE/setup
 
